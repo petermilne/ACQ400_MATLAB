a4ca4f8ed1dece525919a2572ecebff810b57993
Scott Robson <scott.robson@d-tacq.co.uk>
2014-07-10 12:11:01 +0100

    Implemented "shot_complete" test to avoid stale data

--- trans_cap.m
+++ trans_cap.m
@@ -1,7 +1,9 @@
 function trans_cap(num_samp,num_ch)
+%tic
     global UUT %Make base workspace variable visible in function
     disp(UUT)
-    vsf = 10/2^16; % Voltage Scaling Factor
+    vsf = 20/2^16; % Voltage Scaling Factor
+    samp_rate = 48000; % Sample rate
     
     ID = tcpip(UUT,4220); % 4220 = System Controller
     ID.terminator = 10; % ASCII line feed
@@ -12,6 +14,7 @@ function trans_cap(num_samp,num_ch)
     command = 'shot_complete';
     fprintf(ID,command); % Queries the value of shot_complete on UUT
     pre = fscanf(ID); % Map response of query to 'pre'
+    pre = str2double(pre);
     %disp(pre)
     
     command = sprintf('soft_transient %s',1000000);
@@ -19,11 +22,11 @@ function trans_cap(num_samp,num_ch)
     readback = fscanf(ID);
     fprintf('%s',readback);
     
-    
+
     %% Poll shot_complete
     %  Map result to POST. When it increments, and POST is
     %  one greater than PRE loop breaks.
-    command = 'shot';
+    command = 'shot_complete';
     while true
         fprintf(ID,command);
         post = fscanf(ID);
@@ -40,14 +43,10 @@ function trans_cap(num_samp,num_ch)
     delete(ID);
     
     %% Pull transient data from channels 53001:53032
-    %  Store results in array indexed 1:32
-    
-    pause(2)%temporary wait, this would normally be served by the WHILE above
-    
+    %  Store results in array indexed 1:32  
     clear CHx
-    disp('...Pulling Channel Data from D-TACQ ACQ...');
+    fprintf('...Pulling Channel Data from D-TACQ ACQ...\n');
     for i=1:num_ch
-        
         channel=53000+i;
         disp(i);
         CH = tcpip(UUT,channel);
@@ -59,7 +58,7 @@ function trans_cap(num_samp,num_ch)
         
         CHx{i} = fread(CH,num_samp,'int16');
         
-        % If you wish you can save data to binary file for posterity
+        % If you wish you can save channel data to binary file for posterity
         %filename = sprintf('%s_%02d.bin',UUT,i);
         %f = fopen(filename,'w');
         %fwrite(f,CHx{i},'int32',0,'b');
@@ -67,10 +66,7 @@ function trans_cap(num_samp,num_ch)
         
         fclose(CH);
         delete(CH);
-        
     end
-    
-    
     
     whos CHx
     fprintf('\n...Data Transfer Complete...\n\n');
@@ -82,19 +78,24 @@ function trans_cap(num_samp,num_ch)
     hold all
     
     for i=1:num_ch
-    CHx{i} = CHx{i}.*vsf;
+    CHx{i} = CHx{i}.*vsf; % Scale to volts
     end
+    
+    index = 1:num_samp;
+    %index = index./samp_rate; %Uncomment this line for seconds on x-axis
     
     fig1 = figure(1);
     for i=1:num_ch
-        plot(CHx{i})
+        plot(index, CHx{i})
     end
     
     %title('Transient Capture') 
-    %xlabel('# of Samples');
-    %ylabel('Volts');
+    xlabel('Samples');
+    %xlabel('Seconds');
+    ylabel('Volts');
     %legend('sig1','sig2')
     hold off
     set(fig1,'units','normalized','outerposition',[0 0 1 1]); % MATLABs best approximation of maximising figure window.
     shg
+%toc
 end
--- trans_cap32.m
+++ trans_cap32.m
@@ -2,7 +2,8 @@ function trans_cap32(num_samp,num_ch)
 %tic
     global UUT %Make base workspace variable visible in function
     disp(UUT)
-    vsf = 10/2^32; % Voltage Scaling Factor
+    vsf = 20/2^32; % Voltage Scaling Factor
+    samp_rate = 48000; % Sample rate
     
     ID = tcpip(UUT,4220); % 4220 = System Controller
     ID.terminator = 10; % ASCII line feed
@@ -10,7 +11,7 @@ function trans_cap32(num_samp,num_ch)
     ID.Timeout = 60;
     fopen(ID);
     
-    command = 'shot';
+    command = 'shot_complete';
     fprintf(ID,command); % Queries the value of shot_complete on UUT
     pre = fscanf(ID); % Map response of query to 'pre'
     pre = str2double(pre);
@@ -22,10 +23,10 @@ function trans_cap32(num_samp,num_ch)
     fprintf('%s',readback);
     
 
-    %% Poll shot
+    %% Poll shot_complete
     %  Map result to POST. When it increments, and POST is
     %  one greater than PRE loop breaks.
-    command = 'shot';
+    command = 'shot_complete';
     while true
         fprintf(ID,command);
         post = fscanf(ID);
@@ -42,14 +43,10 @@ function trans_cap32(num_samp,num_ch)
     delete(ID);
     
     %% Pull transient data from channels 53001:53032
-    %  Store results in array indexed 1:32
-    
-    %pause(5)%temporary wait, this would normally be served by the WHILE above
-    
+    %  Store results in array indexed 1:32  
     clear CHx
-    fprintf('...Pulling Channel Data from D-TACQ ACQ...');
+    fprintf('...Pulling Channel Data from D-TACQ ACQ...\n');
     for i=1:num_ch
-        
         channel=53000+i;
         disp(i);
         CH = tcpip(UUT,channel);
@@ -61,7 +58,7 @@ function trans_cap32(num_samp,num_ch)
         
         CHx{i} = fread(CH,num_samp,'int32');
         
-        % If you wish you can save data to binary file for posterity
+        % If you wish you can save channel data to binary file for posterity
         %filename = sprintf('%s_%02d.bin',UUT,i);
         %f = fopen(filename,'w');
         %fwrite(f,CHx{i},'int32',0,'b');
@@ -69,7 +66,6 @@ function trans_cap32(num_samp,num_ch)
         
         fclose(CH);
         delete(CH);
-        
     end
     
     whos CHx
@@ -82,17 +78,21 @@ function trans_cap32(num_samp,num_ch)
     hold all
     
     for i=1:num_ch
-    CHx{i} = CHx{i}.*vsf;
+    CHx{i} = CHx{i}.*vsf; % Scale to volts
     end
+    
+    index = 1:num_samp;
+    %index = index./samp_rate; %Uncomment this line for seconds on x-axis
     
     fig1 = figure(1);
     for i=1:num_ch
-        plot(CHx{i})
+        plot(index, CHx{i})
     end
     
     %title('Transient Capture') 
-    %xlabel('# of Samples');
-    %ylabel('Volts');
+    xlabel('Samples');
+    %xlabel('Seconds');
+    ylabel('Volts');
     %legend('sig1','sig2')
     hold off
     set(fig1,'units','normalized','outerposition',[0 0 1 1]); % MATLABs best approximation of maximising figure window.
