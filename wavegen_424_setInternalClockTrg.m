95e0aff2cad91701ab6337e6a01ecd82d8d16d8b
Peter Milne <peter.milne@d-tacq.com>
2014-12-02 16:21:34 +0000

    global clocking setup 	modified:   wavegen_424.m 	new file:   wavegen_424_setInternalClockTrg.m

--- wavegen_424_setInternalClockTrg.m
+++ wavegen_424_setInternalClockTrg.m
@@ -1,10 +1,8 @@
-%% wavegen_424.m    
-% function initialises the ao424 awg using the "wavegen" command
+%% wavegen_424_setInternalClockTrg
+% function sets up the ao424 awg for internal clock and soft trigger
 % Inputs:
 % site : 1..6
-% nchan: 1..32
-% loop : 0 = one-shot, 1 = forever
-% soft_trigger : 0 = no-trigger, 1=trigger
+% freq : sample rate in Hz
 %
 % Assumes that channel data files have been uploaded previously using upload_ch
 % Queues AWG data files to the AWG device
@@ -13,22 +11,33 @@
 % to use the trigger on starting an adjacent ADC module
 %
 
-function wavegen_424(site,nchan,loop,_soft_trigger)
+function wavegen_424_setInternalClockTrg(site,hz)
     global UUT %Make base workspace variable visible in function
  
-    if nargin < 4; _soft_trigger = 0; end;
-    if nargin < 3; loop = 0; end
-    if nargin < 2; nchan = 32; end;
+    if nargin < 2; hz = 100000; end;
     if nargin < 1; site = 1; end;
-    
+   
+    if hz < 1000; hz = 1000; end;
+    if hz > 500000; hz = 500000; end;
+ 
     ID = tcpip(UUT,4220+site); % Assuming AO card in Site 1
     ID.terminator = 10; % ASCII line feed
     ID.InputBufferSize = 100;
     ID.Timeout = 60;
     fopen(ID);
     
-    fprintf(ID,'wavegen --loop %d 1:%d=ch%02d\n', loop, nchan);
-    
+    % soft trigger
+    fprintf(ID, 'set.trig 1,1,1\n');
+    readback = fscanf(ID);
+    disp(readback)
+   
+    % internal clock 
+    fprintf(ID, 'set.clk 0,0,0\n');
+    readback = fscanf(ID);
+    disp(readback)
+   
+    % rate N = 66MHz/Hz
+    fprintf(ID, 'clkdiv=%d\n', floor(66000000/hz));
     readback = fscanf(ID);
     disp(readback)
 
