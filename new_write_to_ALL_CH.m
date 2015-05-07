ed71cb941550a7964bb7dfb18181f5e37c09f9ae
Scott Robson <scott.robson@d-tacq.co.uk>
2015-05-07 10:40:11 +0100

    Fixed name of function in new_write_to_ALL_CH

--- new_write_to_ALL_CH.m
+++ new_write_to_ALL_CH.m
@@ -1,7 +1,7 @@
 %% write_to_ALL_CH.m
 % Simple function that calls |write_to_CH| four times to write data to 4
 % ports corresponding to 4CH of AWG.
-function write_to_ALL_CH(CH_data)
+function new_write_to_ALL_CH(CH_data)
     
     for i=1:length(CH_data)
         write_to_CH(CH_data{i},i);
