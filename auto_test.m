9dee73a5a9897b59231ffad8575bc04e1cc79d7f
Scott Robson <scott.robson@d-tacq.co.uk>
2015-07-13 14:25:54 +0100

    New auto_test to validate acq424

--- auto_test.m
+++ auto_test.m
@@ -3,15 +3,16 @@ function auto_test(num_runs)
 
 freq = 10000;
 run_count=0;
+num_samp = 100000;
 
-%for run_count=1:num_runs
-while(1)
+for run_count=1:num_runs
+%while(1)
     run_count = run_count+1;
     run_count
     
     CHx_old = evalin('base','CHx');
     
-    trans_cap('acq437',0,10000,16,'hard',freq)
+    trans_cap('acq424',0,num_samp,32,'soft',freq)
     % Pull in data from workspace
     CHx = evalin('base','CHx');
     
@@ -22,30 +23,21 @@ while(1)
         return;
     end
     
-    pulse_count = nnz(diff(CHx{1} > 1) > 0);
-    
-    % 10 Hz input
-    % Should see 10 pulses at 10 kHZ
-    % Should see 1 pulse at 100 kHz
-    
-    if freq == 10000
-        if pulse_count ~= 10
-            message = sprintf('Failed at run number %d, %d pulses present, should be 10!',run_count,pulse_count);
-            disp(message)
-            %close all
+    for j=1:num_samp
+        for i=1:32
+            temp = CHx{i};
+            temp2(i) = temp(j);
+        end
+        
+        data_avg(1:32) = mean(temp2);
+        plus_ten = data_avg(1)+abs((0.05*(data_avg(1))));
+        minus_ten = data_avg(1)-abs((0.05*(data_avg(1))));
+        
+        if data_avg < minus_ten% || data_avg(1:32) > plus_ten
+            disp('outlier found')
             return;
         end
-        freq = 100000;
-    else
-        if pulse_count ~= 1
-            message = sprintf('Failed at run number %d, %d pulses present, should be 1!',run_count,pulse_count);
-            disp(message)
-            %close all
-            return;
-        end
-        freq = 10000;
     end
-    
     %pause(1)
     %close all
 
